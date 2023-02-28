79acf9a481e907af6a3fec378152e1a0c606e807
archangel913 <archangel.yt.0913@gmail.com>
2023-02-28 10:30:27 +0900

    add methods of Vec2d and Vec3d

--- src/main/GameMath/DoubleVec/Vec3d.java
+++ src/main/GameMath/DoubleVec/Vec3d.java
@@ -11,16 +11,36 @@ public class Vec3d {
         this.z = z;
     }
 
-    public void add(double x, double y, double z){
-        this.x += x;
-        this.y += y;
-        this.z += z;
-    }
-
     public void add(Vec3d other){
         this.x += other.x;
         this.y += other.y;
         this.z += other.z;
+    }
+
+    public void sub(Vec3d other){
+        this.x -= other.x;
+        this.y -= other.y;
+        this.z -= other.z;
+    }
+
+    public void mul(double num){
+        this.x *= num;
+        this.y *= num;
+        this.z *= num;
+    }
+
+    public void div(double num){
+        this.x /= num;
+        this.y /= num;
+        this.z /= num;
+    }
+
+    public double dot(Vec3d other){
+        return this.x * other.x + this.y * other.y + this.z * other.z;
+    }
+
+    public Vec3d cross(Vec3d other){
+        return new Vec3d(this.y * other.z - other.y * this.z, this.x * other.z - other.x * this.z, this.x * other.y - other.x * this.y);
     }
 
     public double getX(){ return this.x; }
